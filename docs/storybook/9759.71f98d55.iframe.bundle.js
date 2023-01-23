bbbd87a917f32fbcf440d4a2895c6be7965f93c9
GitHub Action <action@github.com>
2023-01-23 08:25:11 +0000

    Auto update github page

--- docs/storybook/9759.71f98d55.iframe.bundle.js
+++
@@ -1,3 +0,0 @@
-"use strict";(self.webpackChunk_public_ui_storybook=self.webpackChunk_public_ui_storybook||[]).push([[9759],{"./node_modules/@public-ui/components/dist/esm/kol-button-group-wc.entry.js":(n,u,s)=>{s.r(u),s.d(u,{kol_button_group_wc:()=>t});var o=s("./node_modules/@public-ui/components/dist/esm/index-1c88bb74.js");/*!
- * KoliBri - The accessible HTML-Standard
- */const t=class{constructor(l){(0,o.r)(this,l),this.state={}}render(){return(0,o.h)(o.H,null,(0,o.h)("div",{class:"flex flex-wrap bg-normal"},(0,o.h)("slot",null)))}}}}]);
