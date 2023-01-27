08167ec8bbae9ae1fb559fddf5566c33b0fd0d3f
GitHub Action <action@github.com>
2023-01-27 06:19:25 +0000

    Auto update github page

--- docs/designer/98.js
+++ docs/designer/98.js
@@ -1,2 +1,2 @@
 /*! For license information please see 98.js.LICENSE.txt */
-"use strict";(self.webpackChunk_public_ui_designer=self.webpackChunk_public_ui_designer||[]).push([[98],{80098:(s,e,l)=>{l.r(e),l.d(e,{kol_button_group_wc:()=>u});var r=l(46266);const u=class{constructor(s){(0,r.r)(this,s),this.state={}}render(){return(0,r.h)(r.H,null,(0,r.h)("div",{class:"flex flex-wrap bg-normal"},(0,r.h)("slot",null)))}}}}]);
+"use strict";(self.webpackChunk_public_ui_designer=self.webpackChunk_public_ui_designer||[]).push([[98],{80098:(s,e,l)=>{l.r(e),l.d(e,{kol_button_group_wc:()=>u});var r=l(2786);const u=class{constructor(s){(0,r.r)(this,s),this.state={}}render(){return(0,r.h)(r.H,null,(0,r.h)("div",{class:"flex flex-wrap bg-normal"},(0,r.h)("slot",null)))}}}}]);
