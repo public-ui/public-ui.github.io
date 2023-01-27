595f755609dd9ff523156668be963bced2b9dcdf
Martin Oppitz <github@martinoppitz.com>
2023-01-27 07:15:55 +0100

    build: fix

--- docs/build/assets/js/68b57047.9fbd9240.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_public_ui_website=self.webpackChunk_public_ui_website||[]).push([[1363],{5745:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
--- docs/build/assets/js/814f3328.ab5ff2c4.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_public_ui_website=self.webpackChunk_public_ui_website||[]).push([[2535],{5641:e=>{e.exports=JSON.parse('{"title":"Neuste Beitr\xe4ge","items":[{"title":"Button- und Link-Komponenten","permalink":"/blog/button-und-link-komponenten"},{"title":"\xdcbersetzungen","permalink":"/blog/ubersetzungen"}]}')}}]);
