2de470a5a4c90c763dc433a1883fab51af9f4984
GitHub Action <action@github.com>
2023-02-02 05:31:26 +0000

    Auto update github page

---
+++ docs/assets/js/67d433cd.928e3c58.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_public_ui_website=self.webpackChunk_public_ui_website||[]).push([[4002],{8021:s=>{s.exports=JSON.parse('{"label":"design system","permalink":"/blog/tags/design-system","allTagsPath":"/blog/tags","count":1}')}}]);
