e0481437a61b07c5ab8a4d60f397892da8ea43dd
GitHub Action <action@github.com>
2023-06-02 04:25:24 +0000

    Auto update github page

---
+++ docs/en/assets/js/3cbfe72d.a8c6a023.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkroot=self.webpackChunkroot||[]).push([[1731],{53454:e=>{e.exports=JSON.parse('{"label":"License","permalink":"/en/docs/tags/license","allTagsPath":"/en/docs/tags","count":1,"items":[{"id":"faq","title":"FAQ","description":"Frequently asked questions about KoliBri","permalink":"/en/docs/faq"}]}')}}]);
