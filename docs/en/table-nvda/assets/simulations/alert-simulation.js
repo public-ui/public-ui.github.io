bd78e0ff8511ebdac89c3363bfc4c8026c993cb7
GitHub Action <action@github.com>
2023-07-11 07:13:49 +0000

    Auto update github page

---
+++ docs/en/table-nvda/assets/simulations/alert-simulation.js
@@ -0,0 +1,1 @@
+const on = {  onClose: console.log, }; const timeout = setTimeout(() => {  document.querySelectorAll('kol-alert').forEach((alert) => {   if (Math.random() > 0.5) {    alert._on = on;    alert.setAttribute('_has-closer', '');   }  }); }, 2500); 
