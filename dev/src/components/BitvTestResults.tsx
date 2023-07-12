602ce99d2850d4afb0c4161329b0b4ebee4b091d
Martin Oppitz <github@martinoppitz.com>
2023-07-12 07:19:32 +0200

    chore: add test results

--- dev/src/components/BitvTestResults.tsx
+++ dev/src/components/BitvTestResults.tsx
@@ -1,11 +1,45 @@
 import BrowserOnly from '@docusaurus/BrowserOnly';
-import React, { FC, useEffect } from 'react';
+import { KolTable } from '@public-ui/react';
+import React, { FC, useEffect, useState } from 'react';
 import { getDataFromExcel } from '../shares/fetch-excel';
 
+function parseThemeResult(result: string, col: number) {
+	let erfuellt = 0;
+	let gesamt = 0;
+	for (let i = 2; i < result.length; i++) {
+		gesamt++;
+		switch (result[i][col]) {
+			case 'erfüllt':
+				erfuellt++;
+				break;
+			default:
+				gesamt--;
+		}
+	}
+	return `${erfuellt}/${gesamt}`;
+}
+
+function parseResult(name: string, result: string) {
+	return {
+		component: name,
+		bmf: parseThemeResult(result, 2),
+		zoll: parseThemeResult(result, 4),
+	};
+}
+
 export const BitvTestResult: FC = () => {
+	const [data, setData] = useState([]);
+
 	useEffect(() => {
 		getDataFromExcel()
-			.then((data) => {
+			.then((results) => {
+				console.log(results);
+				const data = [];
+				for (const key in results) {
+					// eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
+					data.push(parseResult(key, results[key]));
+				}
+				setData(data);
 				console.log(data);
 			})
 			.catch((err) => {
@@ -13,7 +47,32 @@ export const BitvTestResult: FC = () => {
 			});
 	}, []);
 
-	return <div className="">[]</div>;
+	return (
+		<KolTable
+			_caption="Übersicht der Testergebnisse der KoliBri-Komponenten"
+			_headers={{
+				horizontal: [
+					[
+						{
+							label: 'Component',
+							key: 'component',
+						},
+						{
+							label: 'BMF',
+							key: 'bmf',
+							textAlign: 'center',
+						},
+						{
+							label: 'Zoll (v2)',
+							key: 'zoll',
+							textAlign: 'center',
+						},
+					],
+				],
+			}}
+			_data={data}
+		/>
+	);
 };
 
 export const BitvTestResults: FC = () => <BrowserOnly>{() => <BitvTestResult />}</BrowserOnly>;
