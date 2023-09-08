7a91b06384f3d7fcb97fbf3ba497e2584a85960c
GitHub Action <action@github.com>
2023-09-08 10:25:01 +0000

    Auto update github page

---
+++ docs/en/assets/components-esm/tooltip-align-23a876df.js
@@ -0,0 +1,1 @@
+/*! * KoliBri - The accessible HTML-Standard */ import{v as validateAlignment}from"./alignment-488ca37b.js";const validateTooltipAlign=(a,i)=>{validateAlignment(a,"_align",i)};export{validateTooltipAlign as v};
--- docs/en/assets/js/59592.34667062.js
+++
@@ -1,1 +0,0 @@
-/*! For license information please see 59592.34667062.js.LICENSE.txt */ "use strict";(self.webpackChunkroot=self.webpackChunkroot||[]).push([[59592],{59592:(t,r,s)=>{s.r(r),s.d(r,{kol_button_group_wc:()=>u});var o=s(75542);const u=class{constructor(t){(0,o.r)(this,t),this.state={}}render(){return(0,o.h)(o.H,null,(0,o.h)("slot",null))}}}}]);
