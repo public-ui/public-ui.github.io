a41acf81469e6b93ad8d77dd9772c0ffb792727a
GitHub Action <action@github.com>
2023-10-27 18:51:00 +0000

    Auto update github page

---
+++ docs/designer/1141.js
@@ -0,0 +1,2 @@
+/*! For license information please see 1141.js.LICENSE.txt */
+"use strict";(self.webpackChunk_public_ui_designer=self.webpackChunk_public_ui_designer||[]).push([[1141],{71141:(i,t,o)=>{o.r(t),o.d(t,{kol_icon_icofont:()=>r});var s=o(90050);const r=class{constructor(i){(0,s.r)(this,i),this._ariaLabel=void 0,this._icon=void 0,this._part=void 0}render(){return(0,s.h)("kol-icon",{exportparts:"icon"+("string"==typeof this._part?`,${this._part}`:""),_ariaLabel:this._ariaLabel,_icon:"string"==typeof this._icon?`icofont-${this._icon}`:void 0})}}}}]);
