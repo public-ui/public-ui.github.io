e019ca59d72a0f550afd8bc1acf6322b8663b233
Martin Oppitz <github@martinoppitz.com>
2023-11-14 13:51:48 +0100

    fix: dev

--- dev/src/used-by.ts
+++ dev/src/used-by.ts
@@ -7,31 +7,31 @@ export type UsedBy = {
 };
 
 export const USED_BY: Map<string, UsedBy> = new Map<string, UsedBy>();
-USED_BY.set('bmf', {
-	label: 'Bundesministerium der Finanzen',
-	homepageUrl: 'bundesfinanzministerium.de',
-	logoSrc: 'bundesfinanzministerium.de/SiteGlobals/StyleBundles/Bilder/Farbschema/logo.svg?__blob=normal',
-});
-USED_BY.set('bmi', {
-	label: 'Bundesministerium des Innern, für Bau und Heimat',
-	homepageUrl: 'bmi.bund.de',
-	logoSrc: 'bmi.bund.de/SiteGlobals/Frontend/Images/logo.svg?__blob=normal',
-});
-USED_BY.set('itzbund', {
-	label: 'Informationstechnikzentrum Bund',
-	homepageUrl: 'itzbund.de',
-	logoSrc: 'itzbund.de/SiteGlobals/Frontend/Images/logo.svg?__blob=normal',
-});
-USED_BY.set('gzd', {
-	label: 'Generalzolldirektion',
-	homepageUrl: 'zoll.de',
-	logoSrc: 'zoll.de/SiteGlobals/Frontend/Images/logo.svg?__blob=normal',
-});
-USED_BY.set('bzst', {
-	label: 'Bundeszentralamt für Steuern',
-	homepageUrl: 'www.bzst.de/DE/Home/home_node.html',
-	logoSrc: 'upload.wikimedia.org/wikipedia/commons/thumb/6/60/BZSt_Logo.svg/426px-BZSt_Logo.svg.png',
-});
+// USED_BY.set('bmf', {
+// 	label: 'Bundesministerium der Finanzen',
+// 	homepageUrl: 'bundesfinanzministerium.de',
+// 	logoSrc: 'bundesfinanzministerium.de/SiteGlobals/StyleBundles/Bilder/Farbschema/logo.svg?__blob=normal',
+// });
+// USED_BY.set('bmi', {
+// 	label: 'Bundesministerium des Innern, für Bau und Heimat',
+// 	homepageUrl: 'bmi.bund.de',
+// 	logoSrc: 'bmi.bund.de/SiteGlobals/Frontend/Images/logo.svg?__blob=normal',
+// });
+// USED_BY.set('itzbund', {
+// 	label: 'Informationstechnikzentrum Bund',
+// 	homepageUrl: 'itzbund.de',
+// 	logoSrc: 'itzbund.de/SiteGlobals/Frontend/Images/logo.svg?__blob=normal',
+// });
+// USED_BY.set('gzd', {
+// 	label: 'Generalzolldirektion',
+// 	homepageUrl: 'zoll.de',
+// 	logoSrc: 'zoll.de/SiteGlobals/Frontend/Images/logo.svg?__blob=normal',
+// });
+// USED_BY.set('bzst', {
+// 	label: 'Bundeszentralamt für Steuern',
+// 	homepageUrl: 'www.bzst.de/DE/Home/home_node.html',
+// 	logoSrc: 'upload.wikimedia.org/wikipedia/commons/thumb/6/60/BZSt_Logo.svg/426px-BZSt_Logo.svg.png',
+// });
 // USED_BY.set('kern', {
 // 	label: 'KERN - UX-Standard für die deutsche Verwaltung',
 // 	homepageUrl: 'gitlab.opencode.de/kern-designsystem',
