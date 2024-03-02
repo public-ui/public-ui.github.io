b48a9ecccb9c0c122b2d501e8b939db73e0ab1f4
GitHub Action <action@github.com>
2024-03-02 05:32:15 +0000

    Auto update github page

---
+++ docs/v1/sample-react/3950.js
@@ -0,0 +1,2 @@
+/*! For license information please see 3950.js.LICENSE.txt */
+"use strict";(self.webpackChunk_public_ui_sample_react=self.webpackChunk_public_ui_sample_react||[]).push([[3950],{3950:(e,t,c)=>{c.r(t),c.d(t,{default:()=>l}),setInterval((()=>{const e=document.querySelectorAll("kol-accordion,kol-details");e&&e.forEach((e=>{e.hasAttribute("_open")?e.removeAttribute("_open"):e.setAttribute("_open","")}))}),1e3);const l={}}}]);
