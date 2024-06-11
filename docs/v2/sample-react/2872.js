b02f99e24f5c6797e4923e51761f7d9aa847882d
GitHub Action <action@github.com>
2024-06-11 04:09:58 +0000

    Auto update github page

--- docs/v2/sample-react/2872.js
+++
@@ -1,2 +0,0 @@
-/*! For license information please see 2872.js.LICENSE.txt */
-"use strict";(self.webpackChunk_public_ui_sample_react=self.webpackChunk_public_ui_sample_react||[]).push([[2872],{2872:(e,t,c)=>{c.r(t),c.d(t,{default:()=>l}),setInterval((()=>{const e=document.querySelectorAll("kol-accordion,kol-details");e&&e.forEach((e=>{e.hasAttribute("_open")?e.removeAttribute("_open"):e.setAttribute("_open","")}))}),1e3);const l={}}}]);
