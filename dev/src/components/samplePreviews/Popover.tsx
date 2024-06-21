44c14b6b599ca4a5832842cf01417f351d57cf30
Christian Salat <salat.christian@googlemail.com>
2024-06-21 08:16:06 +0200

    (#6140) Added popover to component list

--- dev/src/components/samplePreviews/Popover.tsx
+++ dev/src/components/samplePreviews/Popover.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { KolImage } from '@public-ui/react';
 
-const Popover = () => <KolImage _src="/assets/samples/tooltip.png" _alt="Sample Image - Popover" _sizes="20vw"/>;
+const Popover = () => <KolImage _src="/assets/samples/split-button.png" _alt="Sample Image - Popover" _sizes="20vw"/>;
 
 export default Popover;
