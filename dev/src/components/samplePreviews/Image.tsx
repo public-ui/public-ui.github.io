120d8d5c2f34955defc8b27e5e0048522218ae7d
Christian Salat <salat.christian@googlemail.com>
2024-07-01 11:33:41 +0200

    (#6140) Removed alt text from component list

--- dev/src/components/samplePreviews/Image.tsx
+++ dev/src/components/samplePreviews/Image.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { KolImage } from '@public-ui/react';
 
-const Image = () => <KolImage _src="/assets/abgrenzung.jpg" _alt="Alt description" _sizes="20vw" />;
+const Image = () => <KolImage _src="/assets/abgrenzung.jpg" _alt="" _sizes="20vw" />;
 
 export default Image;
--- dev/src/components/samplePreviews/SplitButton.tsx
+++ dev/src/components/samplePreviews/SplitButton.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { KolImage } from '@public-ui/react';
 
-const SplitButton = () => <KolImage _src="/assets/samples/split-button.png" _alt="Sample Image - Split-Button" _sizes="20vw"/>;
+const SplitButton = () => <KolImage _src="/assets/samples/split-button.png" _alt="" _sizes="20vw"/>;
 
 export default SplitButton;
