64e3b9667d8e785209f87f4809467e58259cc6a3
Cyrine Ben Romdhane <135605630+anicyne@users.noreply.github.com>
2024-10-21 16:09:30 +0200

    Fix links to reflect selected version

--- src/shares/version.ts
+++ src/shares/version.ts
@@ -1,8 +1,20 @@
-import type VERSIONS from '../../versions.json';
+import VERSIONS from '../../versions.json';
 
 export type Version = (typeof VERSIONS)[number] & 'current';
 
 export function determinateVersionId(docVersion) {
 	if (!docVersion || !docVersion.preferredVersion) return null;
 	return parseFloat(docVersion?.preferredVersion?.name as Version) < 2 ? 'v1' : 'v2';
+}
+
+export function getVersionForUrl(docVersion: { preferredVersion?: { name: string } }) {
+	if (!docVersion || !docVersion.preferredVersion) return null;
+
+	const lastVersion = VERSIONS.reduce((max, ver) => {
+		return parseFloat(ver) > max ? parseFloat(ver) : max;
+	}, 0).toString();
+
+	return docVersion.preferredVersion?.name === 'current' || docVersion.preferredVersion?.name === lastVersion
+		? null
+		: (docVersion.preferredVersion?.name as Version);
 }
