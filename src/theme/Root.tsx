b9d060a57cb2173ad5f6e0cc5235e493c9cb80e9
Martin Oppitz <6279703+deleonio@users.noreply.github.com>
2025-04-28 22:37:14 +0200

    Update translations and improve accessibility in documentation components

--- src/theme/Root.tsx
+++ src/theme/Root.tsx
@@ -1,4 +1,4 @@
-import { register } from '@public-ui/components';
+import { KoliBriDevHelper, register } from '@public-ui/components';
 import { defineCustomElements } from '@public-ui/components/dist/loader';
 import { DEFAULT } from '@public-ui/theme-default';
 import type { FunctionComponent, PropsWithChildren } from 'react';
@@ -10,7 +10,36 @@ export const Root: FunctionComponent<PropsWithChildren> = (props) => {
 			theme: {
 				detect: 'fixed',
 			},
-		}).catch(console.warn);
+		})
+			.then(() => {
+				KoliBriDevHelper.patchTheme(
+					'default',
+					{
+						'KOL-BUTTON-LINK': `
+							:host,
+							button {
+								font-size: inherit;
+							}
+						`,
+						'KOL-ICON': `
+							:host {
+								color: inherit;
+								font-size: inherit;
+							}
+						`,
+						'KOL-LINK': `
+							:host,
+							a {
+								font-size: inherit;
+							}
+						`,
+					},
+					{
+						append: true,
+					}
+				);
+			})
+			.catch(console.warn);
 	}, []);
 
 	return <div id="doc-app">{props.children}</div>;
