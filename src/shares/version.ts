93cb3935cf2bf54212d93781d812479cf042d378
BF150 <franziska.bartsch@iso-gruppe.com>
2025-11-06 09:05:55 +0100

    changed all dev urls to release 3

--- src/shares/version.ts
+++ src/shares/version.ts
@@ -1,2 +1,2 @@
-export const VERSION_ID = 'v2'; // should be V3, but we don't have a V3 sample app deployed yet.
-export const DEV_NETLIFLY_LINK = 'https://develop--kolibri-public-ui.netlify.app'; // should be release-3, but we don't have a release-3 sample app deployed yet.
+export const VERSION_ID = 'v3';
+export const DEV_NETLIFLY_LINK = 'https://release-3--kolibri-public-ui.netlify.app';
