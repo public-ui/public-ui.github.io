b97d224cdd33767506080e2a05a996346710a049
Martin Oppitz <6279703+deleonio@users.noreply.github.com>
2025-12-17 19:02:16 +0100

    Refactor HomepageFeatures and LanguageSwitch to use useBaseUrl for URL management

--- src/components/LanguageSwitch.tsx
+++ src/components/LanguageSwitch.tsx
@@ -1,19 +1,21 @@
 import { useLocation } from '@docusaurus/router';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 import { KolLinkButton } from '@public-ui/react-v19';
 import type { FC } from 'react';
 import React from 'react';
 
 export const LanguageSwitch: FC = () => {
 	const { i18n } = useDocusaurusContext();
-
 	const location = useLocation();
+	const englishUrl = useBaseUrl(`/en${location.pathname}`);
+	const germanUrl = useBaseUrl(location.pathname.replace(/^\/en/, ''));
 
 	return (
 		<div>
 			{i18n.currentLocale === 'de' ? (
 				<KolLinkButton
-					_href={`/en${location.pathname}`}
+					_href={englishUrl}
 					_label="Switch to English"
 					_hideLabel
 					_icons="codicon codicon-globe"
@@ -22,7 +24,7 @@ export const LanguageSwitch: FC = () => {
 				/>
 			) : (
 				<KolLinkButton
-					_href={`${location.pathname.replace(/^\/en/, '')}`}
+					_href={germanUrl}
 					_label="Zu Deutsch wechseln"
 					_hideLabel
 					_icons="codicon codicon-globe"
