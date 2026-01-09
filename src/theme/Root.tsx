bc3d3a7aed3c5088bf44ce984ad2b5459e45e63d
Martin Oppitz <6279703+deleonio@users.noreply.github.com>
2026-01-09 07:55:33 +0100

    feat: replace codicon icons

--- src/theme/Root.tsx
+++ src/theme/Root.tsx
@@ -1,12 +1,14 @@
 import { register } from '@public-ui/components';
 import { defineCustomElements } from '@public-ui/components/loader';
 import { DEFAULT } from '@public-ui/theme-default';
+import type { LoaderCallback } from 'adopted-style-sheets';
 import type { FunctionComponent, PropsWithChildren } from 'react';
 import React, { useEffect } from 'react';
 
 export const Root: FunctionComponent<PropsWithChildren> = (props) => {
 	useEffect(() => {
-		register(DEFAULT, defineCustomElements).catch(console.warn);
+		const loader = defineCustomElements as unknown as LoaderCallback;
+		register(DEFAULT, loader).catch(console.warn);
 	}, []);
 
 	return <div id="doc-app">{props.children}</div>;
