67834955254d8971166ebe9c9141661c6a9ca2a5
Peter Laske <37439758+laske185@users.noreply.github.com>
2026-03-03 13:23:10 +0000

    feat: add HeadingPreview component and update documentation

---
+++ src/components/previews/components/Heading.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Preview, { PreviewLayout } from '../Preview';
+import { LevelProperty } from '../properties';
+import type { JSX } from '@public-ui/components';
+import { KolInputText, KolHeading } from '@public-ui/react-v19';
+
+const HeadingPreview: React.FC = (props: {
+    initialProps?: JSX.KolHeading;
+    visibleProperties?: (keyof JSX.KolHeading)[];
+    codeCollapsable?: boolean;
+    codeCollapsed?: boolean;
+}) => {
+    const defaultProps: JSX.KolHeading = {
+        _label: 'Heading',
+        _level: 1,
+    };
+
+    return (
+        <Preview<JSX.KolHeading>
+            propertyComponents={{
+                _label: <KolInputText _label="Label" />,
+                _level: <LevelProperty label="Level" defaultValue={1} />,
+                _secondaryHeadline: <KolInputText _label="Secondary Headline" />,
+            }}
+            initialProps={{ ...defaultProps, ...props.initialProps }}
+            componentName="KolHeading"
+            visibleProperties={props.visibleProperties}
+            codeCollapsable={props.codeCollapsable}
+            codeCollapsed={props.codeCollapsed}
+            layout={PreviewLayout.CENTERED}
+        >
+            {(componentProps) => <KolHeading {...componentProps} />}
+        </Preview>
+    );
+};
+
+export default HeadingPreview;
