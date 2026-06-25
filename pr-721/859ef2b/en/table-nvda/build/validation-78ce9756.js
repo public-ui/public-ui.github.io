ed6c8a445b4415aa36d2b45f0cab9bd5dfdc6388
ItzGerSte <ItzGerSte@users.noreply.github.com>
2026-06-25 07:28:26 +0000

    Deploying to gh-pages from @ public-ui/public-ui.github.io@859ef2b5bde2677cae2ecf6bc2db97c57294f4c0 🚀

---
+++ pr-721/859ef2b/en/table-nvda/build/validation-78ce9756.js
@@ -0,0 +1,1 @@
+/*! * KoliBri - The accessible HTML-Standard */ import{c as o}from"./a11y.tipps-beae7792.js";import{d as t}from"./prop.validators-0a66a178.js";const s=(s,p,a)=>{t(p,"_links",(o=>typeof o==="object"&&(typeof o._href==="string"||typeof o._label==="string")),a);o(s,p.state._links.length)};export{s as w}; //# sourceMappingURL=validation-78ce9756.js.map
