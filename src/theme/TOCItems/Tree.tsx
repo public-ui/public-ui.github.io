d4a94fd6a0719ce9aa24cf74aa233797bba5befd
ItzGerSte <ItzGerSte@pm.me>
2026-06-25 09:24:58 +0200

    Fix fuer Lint-Fehler

--- src/theme/TOCItems/Tree.tsx
+++ src/theme/TOCItems/Tree.tsx
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, {type ReactNode} from 'react';
 import Link from '@docusaurus/Link';
+import type {Props} from '@theme/TOCItems/Tree';
+import type {TOCTreeNode} from '@docusaurus/theme-common/src/utils/tocUtils.ts';
+
+/*interface headingType {
+  id: string;
+  value: string;
+  children: TOCTreeNode[]
+} */
+
 // Recursive component rendering the toc tree
-function TOCItemTree({toc, className, linkClassName, isChild}) {
+function TOCItemTree({
+  toc,
+  className,
+  linkClassName,
+  isChild,
+}: Props): ReactNode {
   if (!toc.length) {
     return null;
   }
   return (
     <ul className={isChild ? undefined : className}>
       {isChild ? undefined : <>Inhaltsverzeichnis</>}
-      {toc.map((heading) => (
-          <li key={heading.id}>
+      {toc.map((heading:TOCTreeNode) => (
+        <li key={heading.id}>
           <Link
             to={`#${heading.id}`}
             className={linkClassName ?? undefined}
@@ -27,5 +41,6 @@ function TOCItemTree({toc, className, linkClassName, isChild}) {
     </ul>
   );
 }
+
 // Memo only the tree root is enough
 export default React.memo(TOCItemTree);
