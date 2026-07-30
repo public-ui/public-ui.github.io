85ef64b5dcb4770e6004e5472aec9215d0c85f78
ItzGerSte <ItzGerSte@pm.me>
2026-07-30 13:50:45 +0200

    Fix für Single-Select -> Verwendet auch SelectOptionsProperty

--- src/components/previews/components/Select.tsx
+++ src/components/previews/components/Select.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Preview, { PreviewLayout } from '../Preview';
 import { BooleanProperty, MsgProperty, SelectOptionsDefault, SelectOptionsProperty } from '../properties';
-import type { JSX, SelectOption } from '@public-ui/components';
+import type { JSX } from '@public-ui/components';
 import { KolInputNumber, KolInputText, KolSelect } from '@public-ui/react-v19';
 import { translate } from '@docusaurus/Translate';
 
@@ -16,7 +16,7 @@ const SelectPreview = (props: SelectPreviewComponentProps) => {
 	const defaultProps = React.useMemo<JSX.KolSelect>(
 		() => ({
 			_label: translate({ id: 'preview.component.select.label' }),
-			_options: SelectOptionsDefault satisfies SelectOption<string>[],
+			_options: SelectOptionsDefault,
 		}),
 		[]
 	);
