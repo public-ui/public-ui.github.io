54eadf2866a8f126c488d172446b8855adaba48b
deleonio <deleonio@users.noreply.github.com>
2026-08-01 02:47:11 +0000

    Deploying to gh-pages from @ public-ui/public-ui.github.io@2b06f7cfa0f23236def0ff257832b56106a26287 🚀

---
+++ pr-780/2b06f7c/table-nvda/build/validation-441b613f.js
@@ -0,0 +1,5 @@
+/*!
+ * KoliBri - The accessible HTML-Standard
+ */
+import{a as e}from"./prop.validators-0a66a178.js";const r=(r,a)=>{e(r,"_level",(e=>typeof e==="number"&&0<=e&&e<=6),new Set(["Number {0, 1, 2, 3, 4, 5, 6}"]),a,{defaultValue:1,required:true})};export{r as w};
+//# sourceMappingURL=validation-441b613f.js.map
