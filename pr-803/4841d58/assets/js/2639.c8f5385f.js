a695ceb8e1cd3209400c1bc93e62abdfc6f6636d
BF150 <BF150@users.noreply.github.com>
2026-08-10 06:00:31 +0000

    Deploying to gh-pages from @ public-ui/public-ui.github.io@4841d5869e07ef7a1eeacc005ac9ca78eb7d3fb0 🚀

---
+++ pr-803/4841d58/assets/js/2639.c8f5385f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_public_ui_docs_v4=self.webpackChunk_public_ui_docs_v4||[]).push([[2639],{62639:(s,_,c)=>{c.r(_),c.d(_,{default:()=>a});c(12888);var e=c(23806),u=c(11684);const a=()=>(0,u.jsx)(e.I0,{_src:"/assets/samples/modal.png",_alt:"",_sizes:"20vw"})}}]);
