266478174b55d7f582ee56bceba7ad8a4dec91e9
KaNinchen95 <KaNinchen95@users.noreply.github.com>
2026-08-11 12:49:46 +0000

    Deploying to gh-pages from @ public-ui/public-ui.github.io@b3e3477590e2b74a569b0787dd22f2052664bd81 🚀

---
+++ pr-811/b3e3477/en/table-nvda/assets/simulations/pagination-simulation.js
@@ -0,0 +1,1 @@
+var buttons = document.querySelectorAll('kol-pagination'); buttons.forEach((button) => {  button._on = {   onClick: (event, page) => {    alert(`Pagination "${page}" wurde geklickt.`);   },  }; }); 
