194ad65040ec3a065d713b8d47d443267ea80792
deleonio <deleonio@users.noreply.github.com>
2026-08-27 02:42:48 +0000

    Deploying to gh-pages from @ public-ui/public-ui.github.io@f556bbe236022e2ce457403b6942bd4e3006cc4e 🚀

---
+++ pr-817/f556bbe/assets/js/8826.897776b1.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_public_ui_docs_v4=self.webpackChunk_public_ui_docs_v4||[]).push([[8826],{8826:(_,u,a)=>{a.r(u),a.d(u,{default:()=>i});a(71763);var n=a(81498),s=a(91987);const i=()=>(0,s.jsx)(n.Lp,{_max:14,_page:14,_siblingCount:1,_boundaryCount:2,_label:"Pagination",_on:{}})}}]);
